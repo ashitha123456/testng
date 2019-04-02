f11034950ca245818773d0b77c5a0ce52a1b570f
ashitha123456 <ashithashetty064@gmail.com>
2019-04-02 09:51:11 +0530

    hgh

---
+++ src/github/program1.java
@@ -0,0 +1,15 @@
+package github;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class program1 {
+	@Test
+	public void demo() {
+		System.out.println("hiii");
+	}
+     @BeforeMethod
+     public void demo1() {
+    	 System.out.println("hello");
+     }
+}
